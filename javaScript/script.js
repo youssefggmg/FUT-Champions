a8c5e4be8910515130d254f32616cf19162e4677
youssef elhoubi <elhoubiyoussef@gmail.com>
2024-11-28 12:45:14 +0100

    changed the add players in place button for some future work

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,9 +1,13 @@
 const addPlayer = document.querySelector("#addPlayer");
 const popupClose = document.querySelector("#popup-close");
 const addPlayerForm = document.querySelector("#addPlayerForm");
+// fields in the form 
+const playerName = document.querySelector("#playerName");
+const image = document.querySelector("#PlayerImage");
+
 addPlayer.addEventListener("click",()=>{
     addPlayerForm.classList.toggle("hidden")
-    
+
 })
 popupClose.addEventListener("click",()=>{
     addPlayerForm.classList.add("hidden")
